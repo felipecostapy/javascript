9ab743e9008c9ee884d88b018b2a2481682c3b04
Felipe Costa <contatolipecosta10@gmail.com>
2025-09-06 23:36:53 -0300

    06/09

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 2
+
+console.log(`A velocidade do seu carro é ${vel} KM/h`)
+
+if(vel > 60){ // condição simples pois nao contém o else
+     console.log('Você ultrapassou a velocidade permitida. MULTADO!!')
+}
+console.log('Dirija sempre usando o cinto de segurança!')
+
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+console.log(`Vivendo em ${pais}, você é:`)
+if(pais != 'Brasil'){
+     console.log('Estrangeiro')
+} else {
+     console.log('Brasileiro')
+}
