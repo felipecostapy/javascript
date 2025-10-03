1433058d4914cb7039f4c331f3f43d9999f81f17
Felipe Costa <contatolipecosta10@gmail.com>
2025-10-02 21:52:39 -0300

    att

---
+++ aula13 - while/repetições.js
@@ -0,0 +1,23 @@
+var c = 1
+do{
+     console.log(`Passo ${c}`)
+     c++ // c = c + 1
+}
+while(c <= 13)
+
+
+
+
+
+
+
+/*
+var c = 1
+while(c <= 13){
+     console.log(`Passo ${c}`)
+     c++ // c = c + 1
+}
+
+*/
+
+
